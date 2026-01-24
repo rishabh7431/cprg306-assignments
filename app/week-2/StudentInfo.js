f371e58798678a92f73cb718a6915a773940fb24
rishabh7431 <rishabhkaushik33@gmail.com>
2026-01-23 21:43:12 -0700

    week 2 assignment final commit

--- app/week-2/StudentInfo.js
+++ app/week-2/StudentInfo.js
@@ -1,8 +1,8 @@
 export default function StudentInfo() {
   return (
     <main>
-      <h1>Name: Rishabh Kaushik</h1>
-      <h2></h2>
+      <h2>Name: Rishabh Kaushik</h2>
+      <a href="https://github.com/rishabh7431/cprg306-assignments.git">Github: rishabh7431/cprg306-assignments</a>
     </main>
   );
 }
