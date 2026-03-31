e995e0261a91487bda539f35c0f9245f4a287aee
rishabh7431 <rishabhkaushik33@gmail.com>
2026-03-30 22:13:56 -0600

    Fixed build issues

--- app/week-4/item-list.js
+++ app/week-4/item-list.js
@@ -2,12 +2,14 @@ import items from "./items.json";
 
 export function ItemList (){
   return(
-    items.map(item => (
-      <div className = "border border-solid rounded box-content w-full max-w-96 my-5 bg-blue-500 hover:bg-sky-700 py-2" key={item.id}>
-        <p>Name: {item.name}</p>
-        <p>Quantity: {item.quantity}</p>
-        <p>Category: {item.category}</p>
-      </div>
-    )
-  ));
+    <>
+      {items.map(item => (
+        <div className = "border border-solid rounded box-content w-full max-w-96 my-5 bg-blue-500 hover:bg-sky-700 py-2" key={item.id}>
+          <p>Name: {item.name}</p>
+          <p>Quantity: {item.quantity}</p>
+          <p>Category: {item.category}</p>
+        </div>
+      ))}
+    </>
+  );
 }
--- app/week-4/page.js
+++ app/week-4/page.js
@@ -1,12 +1,15 @@
+import React from 'react';
 import { ItemList } from "./item-list";
 
-export default function Page(){
-  return(
+const Page = () => {
+  return (
     <main className="text-center align-middle">
-      <h1 className="text-[36px] font-extrabold ">Shopping List</h1>
+      <h1 className="text-[36px] font-extrabold">Shopping List</h1>
       <div className="grid h-56 grid-cols-3 place-content-evenly">
-        <ItemList/>
+        <ItemList />
       </div>
     </main>
   );
-}
+};
+
+export default Page;
